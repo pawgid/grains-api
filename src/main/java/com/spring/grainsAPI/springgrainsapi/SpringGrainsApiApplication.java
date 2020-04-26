72a270ca52a29b1dff9009825446eb2fba724d9d
pawgid <>
2020-04-26 21:15:37 +0200

    first commit

---
+++ src/main/java/com/spring/grainsAPI/springgrainsapi/SpringGrainsApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.grainsAPI.springgrainsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrainsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrainsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/grainsAPI/springgrainsapi/SpringGrainsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.grainsAPI.springgrainsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrainsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
